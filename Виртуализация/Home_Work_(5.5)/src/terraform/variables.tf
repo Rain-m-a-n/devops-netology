2e81f16280b3cd712e1e5788b065acf867452f1b
Бортников Станислав <bortikovsv@MacBook-Air-BortikovSV.local>
2023-04-13 18:18:37 +0300

    HW_5_5

---
+++ Виртуализация/Home_Work_(5.5)/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1grgef7o02pmmkgrrvd"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gmie6p07gs449h84gj"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd87sa9f9bot0iduo6a7"
+}
