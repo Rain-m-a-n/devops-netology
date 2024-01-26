e05f0ee59593449593af4621f650c4a5f348e1ae
bortnikovsv <bstas2004@gmail.com>
2024-01-26 17:18:32 +0300

    Netology_15.3

---
+++ Проект в облачных провайдерах/15.3/terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "default_zone" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "ssh_adm_key" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "sa" {
+  type       = string
+}
